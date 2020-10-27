ba5df344943e327ed3bbf961f1a3ac9599c2618e
Javel Wilson <javelawilson@gmail.com>
2020-10-27 11:33:33 -0500

    Initialize application

---
+++ src/main/java/com/javelwilson/nyammingsdb/NyammingsDB.java
@@ -0,0 +1,13 @@
+package com.javelwilson.nyammingsdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyammingsDB {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyammingsDB.class, args);
+	}
+
+}
---
+++ src/main/java/com/javelwilson/nyammingsdb/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.javelwilson.nyammingsdb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "wah gwan";
+    }
+}
